5807a7c98e608aff5192dbf10ca301056a66c540
Thibaut <tsepton@gmail.com>
2021-01-11 00:17:25 +0100

    Yes

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Round from "./Round.svelte";
+</script>
+
+<style>
+	main {
+		width: 50rem;
+		height: 50rem;
+		margin: auto;
+	}
+</style>
+
+<main>
+	<Round size={100} />
+</main>
---
+++ src/Round.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { fade } from "svelte/transition";
+
+  export let size;
+
+  let hasChild = false;
+</script>
+
+<style>
+  [container] {
+    display: flex;
+    flex-flow: row wrap;
+
+    justify-content: space-evenly;
+    align-content: space-evenly;
+    align-items: center;
+  }
+  .round {
+    height: 100%;
+    width: 100%;
+    border-radius: 100%;
+    background-color: black;
+  }
+</style>
+
+<div in:fade out:fade container style="width: {size}%; height: {size}%;">
+  {#if !hasChild}
+    <div on:mouseenter|preventDefault={() => (hasChild = true)} class="round" />
+  {:else}
+    {#each Array(4) as _}
+      <svelte:self size={50} />
+    {/each}
+  {/if}
+</div>
